a85b7fd89fa7edac8d978ed7a795557b179bbb6d
joeeid <joe.eid2000@gmail.com>
2023-11-17 17:12:21 +0200

    Empty the Text Area after Sending the Email.

--- javascript/main.js
+++ javascript/main.js
@@ -4,6 +4,7 @@ function AdjNavbar(){
 }
 
 function SendEmail(){
-    var EmailBody = document.getElementById("emailInq").value;
-    window.open("mailto:deejoe.lb@gmail.com?Subject=Business Inquiry Ticket&body=" + encodeURI(EmailBody));
+    var EmailBody = document.getElementById("emailInq");
+    window.open("mailto:deejoe.lb@gmail.com?Subject=Business Inquiry Ticket&body=" + encodeURI(EmailBody.value));
+    EmailBody.value = "";
 }
